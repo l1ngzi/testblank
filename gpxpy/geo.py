a4cb4cc1add5c74542a80339e57fb80d9a2a7350
lingzi <erpnext.verify@gmail.com>
2026-06-11 11:18:52 +0800

    synth bug lm_modify__v1ba1k0a

--- gpxpy/geo.py
+++ gpxpy/geo.py
@@ -129,7 +129,7 @@ def length_2d(locations: List["Location"]=[]) -> float:
 
 def length_3d(locations: List["Location"]=[]) -> float:
     """ 3-dimensional length (meters) of locations (it uses latitude, longitude, and elevation). """
-    return length(locations, True)
+    return length(locations, False)
 
 
 def calculate_max_speed(speeds_and_distances: List[Tuple[float, float]], extreemes_percentile: float, ignore_nonstandard_distances: bool) -> Optional[float]:
