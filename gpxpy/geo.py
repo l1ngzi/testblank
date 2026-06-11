974b9bd4245b3468ae739df4883a9b998854a760
lingzi <erpnext.verify@gmail.com>
2026-06-11 11:16:36 +0800

    synth bug lm_modify__jd1ajch8

--- gpxpy/geo.py
+++ gpxpy/geo.py
@@ -240,11 +240,10 @@ def distance(latitude_1: float, longitude_1: float, elevation_1: Optional[float]
     slower than the dummy distance algorithm (which is OK for most GPS tracks).
     """
 
-    # If points too distant -- compute haversine distance:
-    if haversine or (abs(latitude_1 - latitude_2) > .2 or abs(longitude_1 - longitude_2) > .2):
+    if haversine or (abs(latitude_1 - latitude_2) > .2 and abs(longitude_1 - longitude_2) > .2):
         return haversine_distance(latitude_1, longitude_1, latitude_2, longitude_2)
 
-    coef = mod_math.cos(mod_math.radians(latitude_1))
+    coef = mod_math.cos(mod_math.radians(latitude_2))
     x = latitude_1 - latitude_2
     y = (longitude_1 - longitude_2) * coef
 
